d65f57713119bfe46dd931e05a50ffa4246ad7b3
Zeki Ünyıldız <zekiunyildiz@Zeki-Air.home>
2024-01-18 23:10:51 +0300

    project init

---
+++ movie_recs.py
@@ -0,0 +1,19 @@
+import pymongo
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # .env dosyasını yükler
+
+mongodb_uri = os.getenv("MONGODB_URI")  # .env dosyasından MONGODB_URI değişkenini alır
+
+
+client = pymongo.MongoClient(mongodb_uri)
+print(client)
+db = client.sample_mflix
+print("db:",db)
+collection = db.movies
+
+items = collection.find().limit(5)
+
+for item in items:
+    print(item)
