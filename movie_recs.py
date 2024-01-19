e68372e4d6405f7c0353e40e776784084836fc01
Zeki Ünyıldız <zekiunyildiz@gmail.com>
2024-01-19 15:15:49 +0300

    burcularla demo test

--- movie_recs.py
+++ movie_recs.py
@@ -1,10 +1,13 @@
 import pymongo
 from dotenv import load_dotenv
 import os
+import requests
 
 load_dotenv()  # .env dosyasını yükler
 
 mongodb_uri = os.getenv("MONGODB_URI")  # .env dosyasından MONGODB_URI değişkenini alır
+hf_token = os.getenv("HF_TOKEN")
+embedding_url = "https://api-inference.huggingface.co/pipeline/feature-extraction/sentence-transformers/all-MiniLM-L6-v2"
 
 
 client = pymongo.MongoClient(mongodb_uri)
@@ -13,7 +16,15 @@ db = client.sample_mflix
 print("db:",db)
 collection = db.movies
 
-items = collection.find().limit(5)
+def generate_embedding(text: str) -> list[float]:
+    response = requests.post(
+        embedding_url,
+        headers={"Authorization": f"Bearer {hf_token}"},
+        json={"inputs": text})
+    if response.status_code != 200:
+        raise ValueError(f"Request failed with status code {response.status_code}: {response.text}")
+    return response.json()
 
-for item in items:
-    print("item:",item)
+for doc in collection.find({'plot':{"$exists": True}}).limit(50):
+    doc['plot_embedding_hf'] = generate_embedding(doc['plot'])
+    collection.replace_one({'_id': doc['_id']}, doc)
